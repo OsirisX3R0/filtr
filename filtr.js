f3160ba65419f1401af1c3ebbf322a7fa239f8df
Richard Harris <richard@idestini.com>
2019-06-04 17:00:03 -0500

    Added some functionality; does not work

--- filtr.js
+++ filtr.js
@@ -43,10 +43,86 @@ let opacity;
 let saturate;
 let sepia;
 
-if (initStyle == "none") {
-
+const init = () => {
+    brightRange.value = brightness;
+    brightInput.value = brightness;
+    contrastRange.value = contrast;
+    contrastInput.value = contrast;
+    blurRange.value = blur;
+    blurInput.value = blur;
+    shadowColor.value = shadow;
+    xOffsetInput.value = shadowXOffset;
+    xOffsetRange.value = shadowXOffset;
+    yOffsetInput.value = shadowYOffset;
+    yOffsetRange.value = shadowYOffset;
+    shadowBlurRange.value = shadowBlur;
+    shadowBlurInput.value = shadowBlur;
+    grayRange.value = grayscale;
+    grayInput.value = grayscale;
+    hueRange.value = hue;
+    hueInput.value = hue;
+    invertRange.value = invert;
+    invertInput.value = invert;
+    opacityRange.value = opacity;
+    opacityInput.value = opacity;
+    saturateRange.value = saturate;
+    saturateInput.value = saturate;
+    sepiaRange.value = sepia;
+    sepiaInput.value = sepia;
 }
 
 const setFilter = () => {
+    let filter = "";
+    brightness = brightRange.value;
+    contrast = contrastRange.value;
+    blur = blurRange.value;
+    shadow = shadowColor.value;
+    shadowXOffset = xOffsetRange.value;
+    shadowYOffset = yOffsetRange.value;
+    shadowBlur = shadowBlurRange.value;
+    grayscale = grayRange.value;
+    hue = hueRange.value;
+    invert = invertRange.value;
+    opacity = opacityRange.value;
+    saturate = saturateRange.value;
+    sepia = sepiaRange.value;
 
+    filter += `brightness(${brightness / 100}) contrast(${contrast / 100}) blur(${blur}px) drop-shadow(${shadowXOffset}px ${shadowYOffset}px ${shadowBlur}px ${shadow}) grayscale(${grayscale}%) hue-rotate(${hue}deg) invert(${invert}%) opacity(${opacity / 100}) saturate(${saturate}) sepia(${sepia}%)`;
+    image.style.filter = filter;
+
+    brightInput.value = brightness;
+    contrastInput.value = contrast;
+    blurInput.value = blur;
+    xOffsetInput.value = shadowXOffset;
+    yOffsetInput.value = shadowYOffset;
+    shadowBlurInput.value = shadowBlur;
+    grayInput.value = grayscale;
+    hueInput.value = hue;
+    invertInput.value = invert;
+    opacityInput.value = opacity;
+    saturateInput.value = saturate;
+    sepiaInput.value = sepia;
+}
+
+let allRanges = document.querySelectorAll("input[type=range]");
+let allInputs = document.querySelectorAll("input[type=text]");
+allRanges.forEach(r => r.addEventListener("change", setFilter));
+allInputs.forEach(i => i.disabled = "true");
+
+if (initStyle == "none") {
+    brightness = 50;
+    contrast = 50;
+    blur = 0;
+    shadow = "#000000";
+    shadowXOffset = 0;
+    shadowYOffset = 0;
+    shadowBlur = 0;
+    grayscale = 50;
+    hue = 0;
+    invert = 0;
+    opacity = 0;
+    saturate = 0;
+    sepia = 0;
+
+    init();
 }
